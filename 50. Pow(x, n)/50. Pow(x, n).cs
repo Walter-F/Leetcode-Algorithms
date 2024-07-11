d86eabc25f1ca667b7ab046d24173ea97db28223
Vadim Sidorin <55024370+Walter-F@users.noreply.github.com>
2024-07-11 20:41:55 +0300

    Added new solutions

---
+++ 50. Pow(x, n)/50. Pow(x, n).cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public double MyPow(double x, long n) {
+        if(n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+
+        double result = 1;
+        double current_product = x;
+
+        while(n > 0) {
+            if(n % 2 == 1) {
+                result = result * current_product;
+            }
+            current_product = current_product * current_product;
+            n = n / 2;
+        }
+
+        return result;
+    }
+}
